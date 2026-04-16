8fe8bd8ed3025f6cc83e80fe14eb0f654372f892
yerniteja <tejaabp@gmail.com>
2026-04-16 14:31:54 +0530

    feat: register auth routes in fastify app

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,15 @@
 import Fastify from "fastify";
 import cors from "@fastify/cors";
 import jwt from "@fastify/jwt";
+import { authRoutes } from "./modules/auth/auth.route";
+import bcrypt from "bcrypt";
 
 export const buildApp = () => {
   const app = Fastify({
     logger: true,
   });
-
+  bcrypt.hash("123456", 10).then(console.log);
+  
   app.register(cors, {
     origin: true,
   });
@@ -14,6 +17,8 @@ export const buildApp = () => {
   app.register(jwt, {
     secret: process.env.JWT_SECRET as string,
   });
+
+  app.register(authRoutes)
 
   app.get("/health", async () => {
     return { status: "ok" };
